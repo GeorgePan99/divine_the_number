6683bdec8febb3bc9d7226e020fe3e98e3ae2c37
Георгий Панасенко <georgy.panasenko@gmail.com>
2025-05-03 23:21:55 +0700

    add main code

--- divine_the_number/divine_the_number/Program.cs
+++ divine_the_number/divine_the_number/Program.cs
@@ -1,23 +1,87 @@
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+
 var builder = WebApplication.CreateBuilder();
 builder.Configuration.AddJsonFile("config.json");
 var app = builder.Build();
 
 
-int min_value = Convert.ToInt32(app.Configuration["min_value"]);
-int max_value = Convert.ToInt32(app.Configuration["max_value"]);
-
+int MIN_VALUE = Convert.ToInt32(app.Configuration["min_value"]);
+int MAX_VALUE = Convert.ToInt32(app.Configuration["max_value"]);
 
 var rand = new Random();
-int mistery_vaue = rand.Next(min_value, max_value + 1);
+int mystery_vaue = rand.Next(MIN_VALUE, MAX_VALUE + 1);
 
-Console.WriteLine($"Введите число от {min_value} до {max_value}");
-
-// тут нужно обрабатывать исключения
-int user_value = Convert.ToInt32(Console.ReadLine());
-
-while (user_value != mistery_vaue)
+void Greeting(int min, int max)
 {
-    Console.WriteLine("Не угадали!");
-    user_value = Convert.ToInt32(Console.ReadLine());
+    string text = $"""
+            Добро пожаловать в игру "Угадай число"!
+        Наши правила невероятно просты: мы загадываем вам число в диапазоне от {min} до {max},
+        а вам предстоит его угадать!
+            Если вы поймете, что вы не в силах больше справляться,
+        вы можете ввести слово "giveup", и таком образом, прекратите свои попытки.
+        А мы, в свою очередь, скажем, какое число загадали.
+            Если вы захотите выйти из игры, напишите "exit".
+        Удачи!
+        """;
+    Console.WriteLine(text);
+    Console.WriteLine();
+    Console.WriteLine("Введите предполагаемое число");
 }
-Console.WriteLine("Вы молодец! Число угадано!");
+
+void IsValue(int user_value)
+{
+    if (user_value == mystery_vaue)
+    {
+        Console.WriteLine("Вы угадали число! Вы молодец!");
+        Process.GetCurrentProcess().Kill();
+    }
+    else if (user_value > MAX_VALUE || user_value < MIN_VALUE)
+    {
+        Console.WriteLine("Число вне заданных границ!");
+        Input();
+    }
+    else
+    {
+        Console.WriteLine("Неверно:(");
+        Input();
+    }
+}
+
+void IsCommand(string user_string)
+{
+    if (user_string == "giveup")
+    {
+        Console.WriteLine("В следующий раз повезет!");
+        Console.WriteLine($"Загаданное число: {mystery_vaue}");
+        Process.GetCurrentProcess().Kill();
+    }
+    else if (user_string == "exit")
+    {
+        Console.WriteLine("Хорошего дня!");
+        Process.GetCurrentProcess().Kill();
+    }
+    else
+    {
+        Console.WriteLine("Введенное вами значение не подходит. Попробуйте еще раз");
+        Input();
+    }
+}
+
+
+void Input()
+{
+    string user_input = Console.ReadLine();
+    try
+    {
+        IsValue(Convert.ToInt32(user_input));
+
+    }
+    catch
+    {
+        IsCommand(user_input);
+    }
+}
+
+Greeting(MIN_VALUE, MAX_VALUE);
+Input();
--- divine_the_number/divine_the_number/obj/Debug/net9.0/divine_the_number.AssemblyInfo.cs
+++ divine_the_number/divine_the_number/obj/Debug/net9.0/divine_the_number.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("divine_the_number")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+197d6ea4280c7cceea187c7e322007258ea80a72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f669cb214df6fa4bda6359f50eceb5fffa85817")]
 [assembly: System.Reflection.AssemblyProductAttribute("divine_the_number")]
 [assembly: System.Reflection.AssemblyTitleAttribute("divine_the_number")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
