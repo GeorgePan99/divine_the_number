42714843994be8f1bdc0d7667941a1c8067dccc5
Георгий Панасенко <georgy.panasenko@gmail.com>
2025-08-14 23:03:21 +0700

    remove useless subs

--- DivineNumber/DivineNumber.Business/Registration.cs
+++ DivineNumber/DivineNumber.Business/Registration.cs
@@ -2,40 +2,7 @@
 using DivineNumber.Services.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using System.Globalization;
 
-
-//var config = new ConfigurationBuilder()
-//    .AddJsonFile("appsettings.json")
-//    .AddEnvironmentVariables()
-//    .Build();
-
-//var services = new ServiceCollection()
-//    .AddTransient<ICommandHandler, CommandValidator>()
-//    .AddTransient<IValueValidator, ValueValidator>()
-//    .AddTransient<IComparator, Comparator>()
-//    .AddTransient<IExiter, Exiter>()
-//    .AddLogging()
-//    .AddTransient<ValueRange>()
-//    .AddTransient<LanguageField>()
-//    .AddTransient<IExecution, Execution>()
-//    .AddLocalization(options =>
-//    {
-//        options.ResourcesPath = "Resources";
-//    });
-
-//services.Configure<ValueRange>(config.GetSection(key: "Range"));
-//services.Configure<LanguageField>(config.GetSection(key: "Language"));
-
-
-//using var serviceProvider = services.BuildServiceProvider();
-
-//var languageService = serviceProvider.GetService<IOptions<LanguageField>>();
-
-//CultureInfo.CurrentUICulture = new CultureInfo(languageService.Value.Language);
-
-//namespace Services;
 
 public static class ClearDI
 {
--- DivineNumber/DivineNumber.UI/Program.cs
+++ DivineNumber/DivineNumber.UI/Program.cs
@@ -5,12 +5,10 @@ using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
 
-// Регистрируем сервисы из бизнес-слоя
 services.AddServices();
 
 var provider = services.BuildServiceProvider();
 
-// Получаем сервис
 var greetingService = provider.GetRequiredService<IExecution>();
 
 greetingService.Greetings();
